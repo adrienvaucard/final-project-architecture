34b6fccca0f4fc423451136a2501e8ccef1a878c
Kwoak <maisonneuve.henry@gmail.com>
2021-11-25 16:46:59 +0100

    feat(front home): add profile

--- app/src/app/components/home/home.component.ts
+++ app/src/app/components/home/home.component.ts
@@ -7,19 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
   public questionsNumber: '5' | '10' | '20' = '5';
   public theme: 'burgerQuiz' | 'other' = 'burgerQuiz';
   public loading = false;
 
+  public gameNumber: string = '0';
+  public goodAnswers: string = '0';
+  public averageScore: string = '0';
+
+  constructor() { }
+
+
+
   ngOnInit(): void {
+    this.getProfile()
   }
 
   public play() {
     console.log(this.questionsNumber, this.theme)
     this.loading = true;
     // launch game with questionsNumber and theme
+  }
+
+  public getProfile() {
+    //to do
   }
 
 }
